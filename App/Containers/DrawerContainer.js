dfc6ef18cef58a43b36b6f861061d2efd941e384
Sachin Dev S <hellosachindev@gmail.com>
2017-12-10 00:35:07 +0530

    Added The Notification Drawer & its Design

--- App/Containers/DrawerContainer.js
+++ App/Containers/DrawerContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image,TouchableHighlight } from 'react-native'
 import { NavigationActions } from 'react-navigation'
 
 export default class DrawerContainer extends React.Component {
@@ -19,47 +19,210 @@ export default class DrawerContainer extends React.Component {
     const { navigation } = this.props
     return (
       <View style={styles.container}>
-        <Text
-          onPress={() => navigation.navigate('screen1')}
-          style={styles.uglyDrawerItem}>
-          Screen 1
-        </Text>
-        <Text
-          onPress={() => navigation.navigate('screen2')}
-          style={styles.uglyDrawerItem}>
-          Screen 2
-        </Text>
-        <Text
-          onPress={() => navigation.navigate('screen3')}
-          style={styles.uglyDrawerItem}>
-          Screen 3
-        </Text>
-        <Text
-          onPress={this.logout}
-          style={styles.uglyDrawerItem}>
-          Log Out
-        </Text>
-      </View>
+          <View style={styles.subcontainer}>
+
+              <Image
+                  style={{width: 60, height: 60,borderRadius:30}}
+                  source={require('../../assets/icons/propic.png')}
+                />
+                <Text>
+                <Text style={{fontWeight:'bold',fontSize: 20,paddingTop:8}}>
+                Sachin Dev. S{'\t\t\t\t\t'}
+                </Text>
+                <Image
+                    style={{width: 15, height: 15}}
+                    source={require('../../assets/icons/arrow.png')}
+                  />
+                </Text>
+
+                <Text style={{fontSize: 15,color:'grey',paddingTop: 5}}>
+                @Sachin_Dev_S
+                </Text>
+                <Text style ={{paddingTop:15}}>
+
+                    <Text style={{fontWeight: 'bold',fontSize: 17}}>
+                    690{' '}
+                    </Text>
+
+                    <Text style={{fontSize: 17}}>
+                    Following{'   '}
+                    </Text>
+
+                    <Text style={{fontWeight: 'bold',fontSize: 17}}>
+                    653{' '}
+                    </Text>
+
+                    <Text style={{fontSize: 17}}>
+                    Followers
+                    </Text>
+
+                </Text>
+            </View>
+
+            <View
+                style={{
+                  marginBottom:10,
+                  borderBottomColor: 'grey',
+                  borderBottomWidth: StyleSheet.hairlineWidth,
+                  marginLeft:0,
+                  marginRight:0
+                }}
+            />
+        <View style={styles.subcontainer1}>
+
+        <Text style={{paddingTop:1}}/>
+
+        <Text>
+
+                  <Image
+                    style={{width: 30, height: 30}}
+                    source={require('../../assets/icons/profile.png')}
+
+                    />
+
+                  <Text style={{fontSize: 17,textAlign:'center'}}
+                  onPress={() => navigation.navigate('screen1')}>
+
+                  {'      '}Profile
+                  </Text>
+            </Text>
+            <Text style={{paddingTop:1}}/>
+
+            <Text>
+                  <Image
+                  style={{width: 30, height: 30}}
+                  source={require('../../assets/icons/list.png')}
+
+                    />
+
+                  <Text style={{fontSize: 17,textAlign:'center'}}
+                  onPress={() => navigation.navigate('screen2')}
+                  >
+                  {'      '}List
+                  </Text>
+            </Text>
+            <Text style={{paddingTop:1}}/>
+
+            <Text>
+                  <Image
+                  style={{width: 30, height: 30}}
+
+                  source={require('../../assets/icons/moments.png')}
+
+                    />
+
+                  <Text style={{fontSize: 17,textAlign:'center'}}
+                  onPress={() => navigation.navigate('screen3')}
+                  >
+                  {'      '}Moments
+                  </Text>
+            </Text>
+            <Text style={{paddingTop:1}}/>
+
+            <Text style={{marginBottom:10}}>
+                  <Image
+                  style={{width: 30, height: 30}}
+
+                  source={require('../../assets/icons/highlights.png')}
+
+                    />
+
+                  <Text style={{fontSize: 17,textAlign:'center'}}
+                onPress={this.logout}
+                  >
+                  {'      '}Highlights
+                  </Text>
+            </Text>
+
+        </View>
+            <View
+                style={{
+
+                  borderBottomColor: 'grey',
+                  borderBottomWidth: 1,
+                }}
+            />
+
+            <View style={styles.subcontainer}>
+
+            <Text style={{fontSize: 20,paddingBottom:20,paddingTop:10}}>
+            Settings and Privacy
+            </Text>
+            <Text style={{fontSize: 20}}>
+            Help center
+            </Text>
+            </View>
+            <View style={styles.subcontainer}/>
+
+                <View
+                    style={{
+
+                      borderBottomColor: 'grey',
+                      borderBottomWidth: StyleSheet.hairlineWidth,
+
+
+                      marginLeft:0,
+                      marginRight:0
+                    }}
+                />
+
+
+                <View style={styles.subcontainer}>
+
+
+                <Text style={{marginBottom:10}}>
+                      <Image
+                      style={{width: 30, height: 30}}
+                      resizeMode="contain"
+                      source={require('../../assets/icons/moon.png')}
+
+
+                        />
+                        <Text>{"\t\t\t\t\t\t\t"}</Text>
+
+                        <Image
+                        style={{width: 30, height: 30}}
+                        resizeMode="contain"
+
+                        source={require('../../assets/icons/qr.png')}
+
+
+                          />
+                          </Text>
+            </View>
+
+        </View>
+
+
+
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  subcontainer: {
+    backgroundColor: '#f6f6f6',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    paddingBottom:20,
+  },subcontainer1: {
+    backgroundColor: '#f6f6f6',
+    paddingHorizontal: 20
+  },container: {
     flex: 1,
     backgroundColor: '#f6f6f6',
-    paddingTop: 40,
-    paddingHorizontal: 20
+
   },
-  uglyDrawerItem: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#E73536',
-    padding: 15,
-    margin: 5,
-    borderRadius: 2,
-    borderColor: '#E73536',
-    borderWidth: 1,
-    textAlign: 'center'
-  }
+
+  // uglyDrawerItem: {
+  //   fontSize: 18,
+  //   fontWeight: 'bold',
+  //   color: '#E73536',
+  //   padding: 15,
+  //   margin: 5,
+  //   borderRadius: 2,
+  //   borderColor: '#E73536',
+  //   borderWidth: 1,
+  //   textAlign: 'center'
+  // }
 })
--- android/app/src/main/java/com/tricodia/MainApplication.java
+++ android/app/src/main/java/com/tricodia/MainApplication.java
@@ -3,6 +3,9 @@ package com.tricodia;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeI18n(),
+            new RNDeviceInfo()
       );
     }
 
